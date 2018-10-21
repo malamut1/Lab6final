965709edd5b5e666f827fb9a1ab7c2a48f021c89
Danielle Malamut <dmalamut31@gmail.com>
2018-10-21 15:24:46 -0400

    update

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MvcMovie.Controllers
     {
         public IActionResult Index()
         {
-            return Redirect("Movies");
+            return View();
         }
 
         public IActionResult About()
@@ -29,6 +29,12 @@ namespace MvcMovie.Controllers
             return View();
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
