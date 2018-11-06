69b6ec7653eb819fd38e92f8c652fe68bbf3fd9f
Danielle Malamut <dmalamut31@gmail.com>
2018-11-06 17:30:18 -0500

    Added model

---
+++ MvcMovie/Models/ReviewsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class ReviewsViewModel
+    {
+        public int ID { get; set; }
+
+        public List<Movie> Movies;
+        public SelectList Reviews;
+        public string Reviewer { get; set; }
+        public string Comment { get; set; }
+        public string Title { get; set; }
+    }
+}
