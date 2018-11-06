84cbdcb2575000711723a095948f750b7f188578
Danielle Malamut <dmalamut31@gmail.com>
2018-11-06 17:52:01 -0500

    lab update

---
+++ MvcMovie/Controllers/ReviewsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly MvcMovieContext _context;
+
+        public ReviewsController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reviews
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ReviewsViewModel.ToListAsync());
+        }
+
+        // GET: Reviews/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reviewsViewModel = await _context.ReviewsViewModel
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (reviewsViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(reviewsViewModel);
+        }
+
+        // GET: Reviews/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Reviewer,Comment, Title")] ReviewsViewModel reviewsViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reviewsViewModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reviewsViewModel);
+        }
+
+        // GET: Reviews/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reviewsViewModel = await _context.ReviewsViewModel.FindAsync(id);
+            if (reviewsViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(reviewsViewModel);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Reviewer,Comment, Title")] ReviewsViewModel reviewsViewModel)
+        {
+            if (id != reviewsViewModel.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reviewsViewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewsViewModelExists(reviewsViewModel.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reviewsViewModel);
+        }
+
+        // GET: Reviews/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reviewsViewModel = await _context.ReviewsViewModel
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (reviewsViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(reviewsViewModel);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var reviewsViewModel = await _context.ReviewsViewModel.FindAsync(id);
+            _context.ReviewsViewModel.Remove(reviewsViewModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewsViewModelExists(int id)
+        {
+            return _context.ReviewsViewModel.Any(e => e.ID == id);
+        }
+    }
+}
--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,33 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MvcMovie.Models
 {
     public class Movie
     {
+        [Key]
         public int ID { get; set; }
 
-        [StringLength(60, MinimumLength = 3)]
-        [Required]
+
         public string Title { get; set; }
+
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MMMMM d, yyyy}")]
+        [Required(ErrorMessage = "Please enter a valid date")]
         public DateTime ReleaseDate { get; set; }
 
-        public string Genre { get; set; }
 
-        [Range(1, 100)]
+        public string Genre { get; set; }
+        public string Rating { get; set; }
+        public string Poster { get; set; }
+        public string Reviewer { get; set; }
+        public string Comment { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter a valid price")]
         [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
-        public string Rating { get; set; }
-
-        public string Poster { get; set; }
     }
+
 }
